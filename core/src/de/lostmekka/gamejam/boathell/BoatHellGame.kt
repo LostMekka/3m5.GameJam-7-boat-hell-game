324c0a2fddb64b9d5ad2164f8eb5e4cb07712d09
LostMekka <lostmekka@users.noreply.github.com>
2020-05-09 13:03:51 +0200

    render system

--- core/src/de/lostmekka/gamejam/boathell/BoatHellGame.kt
+++ core/src/de/lostmekka/gamejam/boathell/BoatHellGame.kt
@@ -1,6 +1,7 @@
 package de.lostmekka.gamejam.boathell
 
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.assets.AssetManager
 import ktx.app.KtxGame
 
 class BoatHellGame : KtxGame<Screen>() {
@@ -9,3 +10,5 @@ class BoatHellGame : KtxGame<Screen>() {
         setScreen<GamePlayScreen>()
     }
 }
+
+val assetManager = AssetManager()
---
+++ core/src/de/lostmekka/gamejam/boathell/entity/system/RenderSystem.kt
@@ -0,0 +1,24 @@
+package de.lostmekka.gamejam.boathell.entity.system
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.utils.viewport.Viewport
+import de.lostmekka.gamejam.boathell.entity.component.PositionComponent
+import de.lostmekka.gamejam.boathell.entity.component.SpriteComponent
+import ktx.ashley.get
+
+class RenderSystem(
+    var spriteBatch: SpriteBatch,
+    var shapeRenderer: ShapeRenderer,
+    var viewport: Viewport
+) : BaseSystem() {
+    // deliberately has no update method. the render system is called manually.
+    fun render() {
+        spriteBatch.projectionMatrix = viewport.camera.projection
+        for (entity in entities) {
+            val pos = entity[PositionComponent.mapper]!!
+            val sprite = entity[SpriteComponent.mapper]!!.sprite
+            spriteBatch.draw(sprite, pos.x, pos.y)
+        }
+    }
+}
