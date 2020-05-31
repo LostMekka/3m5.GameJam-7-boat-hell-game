76e81bb428c8115596c6fd4322b5d6e891e4d4a0
Gilbert Roehrbein <gilbert.rohrbein@logmein.com>
2020-05-31 16:28:32 +0200

    step6

--- core/src/de/lostmekka/gamejam/boathell/GameplayScreen.kt
+++ core/src/de/lostmekka/gamejam/boathell/GameplayScreen.kt
@@ -2,7 +2,7 @@ package de.lostmekka.gamejam.boathell
 
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
-import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.ashley.core.Family
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputAdapter
@@ -12,13 +12,33 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import de.lostmekka.gamejam.boathell.asset.Music
 import de.lostmekka.gamejam.boathell.entity.Ships
-import de.lostmekka.gamejam.boathell.entity.component.RenderComponent
+import de.lostmekka.gamejam.boathell.entity.component.PlayerControlledComponent
 import de.lostmekka.gamejam.boathell.entity.component.TransformComponent
 import de.lostmekka.gamejam.boathell.entity.system.*
 import ktx.app.KtxScreen
 import ktx.ashley.*
 import ktx.box2d.createWorld
-import kotlin.math.sin
+
+class CameraControl : BaseSystem() {
+    override fun familyBuilder(): Family.Builder {
+        return allOf(PlayerControlledComponent::class, TransformComponent::class)
+    }
+
+    override fun update(deltaTime: Float) {
+        val player = entities.first()
+        if (player != null) {
+            val trans = TransformComponent.mapper[player]
+            val camera = engine.getSystem<RenderSystem>().camera
+
+            camera.position.add(
+                0.033f * (trans.x - camera.position.x),
+                0.033f * (trans.y - camera.position.y),
+                0f
+            )
+            camera.update()
+        }
+    }
+}
 
 class GamePlayScreen : KtxScreen {
     private var time = 0f
@@ -26,8 +46,8 @@ class GamePlayScreen : KtxScreen {
     private val physicsWorld = createWorld(Vector2.Zero, true)
     private val physicsDebugRenderer = Box2DDebugRenderer(true, true, false, true, false, true)
     private var player: Entity? = null
+    private val cameraControl = CameraControl()
     private val renderSystem = RenderSystem()
-    private val particleSystem = StupidParticleSystem()
 
     val engine = Engine().apply {
         player = Ships.addPlayerBoat(this, physicsWorld)
@@ -41,11 +61,12 @@ class GamePlayScreen : KtxScreen {
         // controllers
         addSystem(PlayerControlledBoatSystem())
         addSystem(AIShipSystem())
+        addSystem(cameraControl)
 
         // special
         addSystem(WaterLayerSystem())
         addSystem(StupidWaterSpawn())
-        addSystem(particleSystem)
+        addSystem(StupidParticleSystem())
         addSystem(renderSystem)
         addSystem(PhysicsUpdateSystem(physicsWorld))
         addSystem(EnemySpawnerSystem(physicsWorld))
@@ -79,7 +100,6 @@ class GamePlayScreen : KtxScreen {
 
     override fun render(delta: Float) {
         time += delta
-        cameraFollowsPlayer()
         engine.update(delta)
     }
 
@@ -89,22 +109,6 @@ class GamePlayScreen : KtxScreen {
     }
 
     override fun dispose() {
-    }
-
-    fun cameraFollowsPlayer() {
-        val player = player
-        if (player != null) {
-            val pos = player.get<TransformComponent>()
-            if (pos != null) {
-                val camera = engine.getSystem<RenderSystem>().camera
-                camera.position.add(
-                    0.033f * (pos.x - camera.position.x),
-                    0.033f * (pos.y - camera.position.y),
-                    0f
-                )
-                camera.update()
-            }
-        }
     }
 
     private fun draw() {
