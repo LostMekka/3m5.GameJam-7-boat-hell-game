02ab33fefa8478ae2dc4d72c56cd4fb8de68abdb
Gilbert Roehrbein <gilbert.rohrbein@logmein.com>
2020-06-01 00:38:37 +0200

    move ship AI into AIShipSystem file

--- core/src/de/lostmekka/gamejam/boathell/entity/Ships.kt
+++ core/src/de/lostmekka/gamejam/boathell/entity/Ships.kt
@@ -7,7 +7,7 @@ import com.badlogic.gdx.physics.box2d.World
 import de.lostmekka.gamejam.boathell.asset.Sounds
 import de.lostmekka.gamejam.boathell.asset.Textures
 import de.lostmekka.gamejam.boathell.asset.toCenteredSprite
-import de.lostmekka.gamejam.boathell.entity.component.AIShipComponent
+import de.lostmekka.gamejam.boathell.entity.system.AIShipComponent
 import de.lostmekka.gamejam.boathell.entity.component.HealthComponent
 import de.lostmekka.gamejam.boathell.entity.component.HitBoxCategory
 import de.lostmekka.gamejam.boathell.entity.component.HitBoxComponent
@@ -18,6 +18,7 @@ import de.lostmekka.gamejam.boathell.entity.component.SoundComponent
 import de.lostmekka.gamejam.boathell.entity.component.RenderComponent
 import de.lostmekka.gamejam.boathell.entity.component.WeaponOwnerComponent
 import de.lostmekka.gamejam.boathell.entity.component.*
+import de.lostmekka.gamejam.boathell.entity.system.AIShipMovementStrategies
 import de.lostmekka.gamejam.boathell.normalizeAngleDeg
 import de.lostmekka.gamejam.boathell.pixels
 import ktx.ashley.get
@@ -123,55 +124,5 @@ object Ships {
             ),
             ShipWaterComp(Vector2((0).pixels, (0).pixels))
         )
-    }
-}
-
-typealias AIMovementStrategy = MovementStrategyContext.() -> Unit
-
-data class MovementStrategyContext(
-    val me: Entity,
-    val target: Entity?
-)
-
-object AIShipMovementStrategies {
-    fun followAndCirculatePlayer(criticalDistance: Float = 4f): AIMovementStrategy = {
-        val playerPos = target?.get(TransformComponent.mapper)
-        val shipPos = TransformComponent.mapper[me]
-        if (target != null && playerPos != null && shipPos != null) {
-            var targetAngle = atan2(playerPos.y - shipPos.y, playerPos.x - shipPos.x) * 180 / PI.toFloat()
-            // if ship is in critical distance -> go around player -> add 90 degree
-            val distance = sqrt((playerPos.x - shipPos.x).pow(2) + (playerPos.y - shipPos.y).pow(2))
-            if (criticalDistance > distance) targetAngle += 90
-
-            val currentAngle = shipPos.rotation
-            val angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
-            val step = 2
-            shipPos.rotation = when {
-                abs(angleDifference) <= step -> targetAngle
-                else -> shipPos.rotation + sign(angleDifference) * step
-            }
-        }
-    }
-
-    fun flyDirectlyToAndAwayFromPlayer(): AIMovementStrategy = {
-        val playerPos = target?.get(TransformComponent.mapper)
-        val shipPos = TransformComponent.mapper[me]
-        if (target != null && playerPos != null && shipPos != null) {
-            var targetAngle = atan2(playerPos.y - shipPos.y, playerPos.x - shipPos.x) * 180 / PI.toFloat()
-            val distance = sqrt((playerPos.x - shipPos.x).pow(2) + (playerPos.y - shipPos.y).pow(2))
-            val currentAngle = shipPos.rotation
-            var angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
-            // if ship is in critical distance -> turn around -> add 180 degree
-            if (4 > distance) targetAngle += 180
-            // if ship is on retreat -> add 180 degree (continue retreat)
-            else if (12 > distance && (-160 > angleDifference || angleDifference > 160)) targetAngle += 180
-
-            angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
-            val step = 2
-            shipPos.rotation = when {
-                abs(angleDifference) <= step -> targetAngle
-                else -> shipPos.rotation + sign(angleDifference) * step
-            }
-        }
     }
 }
--- core/src/de/lostmekka/gamejam/boathell/entity/system/AIShipSystem.kt
+++ core/src/de/lostmekka/gamejam/boathell/entity/system/AIShipSystem.kt
@@ -1,13 +1,17 @@
 package de.lostmekka.gamejam.boathell.entity.system
 
+import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.utils.ImmutableArray
-import de.lostmekka.gamejam.boathell.entity.MovementStrategyContext
-import de.lostmekka.gamejam.boathell.entity.component.AIShipComponent
 import de.lostmekka.gamejam.boathell.entity.component.PlayerControlledComponent
 import de.lostmekka.gamejam.boathell.entity.component.TransformComponent
+import de.lostmekka.gamejam.boathell.entity.component.WeaponOwnerComponent
+import de.lostmekka.gamejam.boathell.normalizeAngleDeg
 import ktx.ashley.allOf
+import ktx.ashley.get
+import ktx.ashley.mapperFor
+import kotlin.math.*
 
 class AIShipSystem : BaseSystem() {
     private lateinit var playerEntities: ImmutableArray<Entity>
@@ -29,4 +33,71 @@ class AIShipSystem : BaseSystem() {
         AIShipComponent::class,
         TransformComponent::class
     )
+}
+
+class AIShipComponent(
+    var movementStrategy: AIMovementStrategy
+) : Component {
+    companion object {
+        val mapper = mapperFor<AIShipComponent>()
+    }
+
+    fun firePattern(me: Entity) {
+        val owner = me[WeaponOwnerComponent.mapper]
+        if (owner != null) {
+            for (weapon in owner.weaponComponents) {
+                weapon.shoot()
+            }
+        }
+    }
+}
+
+typealias AIMovementStrategy = MovementStrategyContext.() -> Unit
+
+data class MovementStrategyContext(
+    val me: Entity,
+    val target: Entity?
+)
+
+object AIShipMovementStrategies {
+    fun followAndCirculatePlayer(criticalDistance: Float = 4f): AIMovementStrategy = {
+        val playerPos = target?.get(TransformComponent.mapper)
+        val shipPos = TransformComponent.mapper[me]
+        if (target != null && playerPos != null && shipPos != null) {
+            var targetAngle = atan2(playerPos.y - shipPos.y, playerPos.x - shipPos.x) * 180 / PI.toFloat()
+            // if ship is in critical distance -> go around player -> add 90 degree
+            val distance = sqrt((playerPos.x - shipPos.x).pow(2) + (playerPos.y - shipPos.y).pow(2))
+            if (criticalDistance > distance) targetAngle += 90
+
+            val currentAngle = shipPos.rotation
+            val angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
+            val step = 2
+            shipPos.rotation = when {
+                abs(angleDifference) <= step -> targetAngle
+                else -> shipPos.rotation + sign(angleDifference) * step
+            }
+        }
+    }
+
+    fun flyDirectlyToAndAwayFromPlayer(): AIMovementStrategy = {
+        val playerPos = target?.get(TransformComponent.mapper)
+        val shipPos = TransformComponent.mapper[me]
+        if (target != null && playerPos != null && shipPos != null) {
+            var targetAngle = atan2(playerPos.y - shipPos.y, playerPos.x - shipPos.x) * 180 / PI.toFloat()
+            val distance = sqrt((playerPos.x - shipPos.x).pow(2) + (playerPos.y - shipPos.y).pow(2))
+            val currentAngle = shipPos.rotation
+            var angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
+            // if ship is in critical distance -> turn around -> add 180 degree
+            if (4 > distance) targetAngle += 180
+            // if ship is on retreat -> add 180 degree (continue retreat)
+            else if (12 > distance && (-160 > angleDifference || angleDifference > 160)) targetAngle += 180
+
+            angleDifference = normalizeAngleDeg(targetAngle - currentAngle)
+            val step = 2
+            shipPos.rotation = when {
+                abs(angleDifference) <= step -> targetAngle
+                else -> shipPos.rotation + sign(angleDifference) * step
+            }
+        }
+    }
 }
